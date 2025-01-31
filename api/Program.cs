63a443ff827e3615bbd8e7bbd5b7d607375a77ae
lupindevv <155176643+lupindevv@users.noreply.github.com>
2025-01-31 14:04:35 +0200

    Update Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -23,6 +23,11 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+var summaries = new[]
+{
+    "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
 app.MapGet("/sensitive", (string? password) =>
 {
     return "Access denied!";
